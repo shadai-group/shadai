b6b37ed4c157fed30b025a46ab1cbe362b91c22d
Jaisir Bayuelo <jaisirenterprise@gmail.com>
2025-01-05 11:57:23 -0500

    version update to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="intelligence",
-    version="0.1.0",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         "boto3==1.35.36",
