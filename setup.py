7a746747379e09296bf57275eda8f18f53c71696
Jaisir Bayuelo <jaisirenterprise@gmail.com>
2025-05-02 04:07:36 -0500

    adjustments for job handling and request, also job polling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="shadai",
-    version="0.1.24",
+    version="0.1.25",
     packages=find_packages(),
     install_requires=[
         "boto3==1.35.36",
